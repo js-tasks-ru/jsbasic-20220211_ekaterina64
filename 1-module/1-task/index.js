be59640fde97efffd32037db81bb853ff409ae4a
evborovik <ekaterina.borovik@t-systems.com>
2022-02-17 14:21:15 +0300

    TASKS for Lesson #1 (1-5)

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,24 @@
+/*
+  Function for evaluation of positive number factorial.
+  n! = n * (n - 1) * (n - 2) * ...*1
+  Examples:
+  0! = 1
+  1! = 1
+  2! = 2 * 1 = 2
+  3! = 3 * 2 * 1 = 6
+  4! = 4 * 3 * 2 * 1 = 24
+  5! = 5 * 4 * 3 * 2 * 1 = 120
+*/
 function factorial(n) {
-  // ваш код...
+  if (n < 0) {
+    throw "Fuction is defined only for positive numbers.";
+  }
+  if (n != 0) {
+    let result = n;
+    for (let i = 1; i < n; i++) {
+      result *= n - i;
+    }
+    return result;
+  }
+  return 1;
 }
--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,13 @@
+/*
+  Function to start word with capital letter
+*/
 function ucFirst(str) {
-  // ваш код...
+  let result = str;
+  if (str.length > 0) {
+    result =
+      str.length === 1
+        ? str.toUpperCase()
+        : str[0].toUpperCase() + str.slice(1);
+  }
+  return result;
 }
