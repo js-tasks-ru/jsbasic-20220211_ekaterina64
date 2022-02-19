2ce78633a42f532748b33a9c60c8028964ba61fc
evborovik <ekaterina.borovik@t-systems.com>
2022-02-19 14:19:19 +0300

    TASKS for Lesson #2 (1-3)

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,20 @@
+/**
+ * Function summarizes numeric values of an object.
+ * @param {*} salaries 
+ * @returns sum  
+ */
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    console.log(key, salaries[key]);
+    if (
+      typeof salaries[key] === "number" &&
+      ![NaN, Infinity, -Infinity].includes(salaries[key])
+    ) {
+      sum += salaries[key];
+    }
+  }
+  const {month, currency, isPayed} = salaries;
+  console.log(`In ${month} sallary of employes is ${sum}${currency}.\nPayed: ${isPayed}`);
+  return sum;
 }
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,12 @@
+/**
+ * Function checks if an object is empty
+ * @param {*} obj 
+ * @returns boolean result
+ */
 function isEmpty(obj) {
-  // ваш код...
+  let keys = Object.keys(obj);
+  if (keys.length > 0) {
+    return false;
+  }
+  return true;
 }
