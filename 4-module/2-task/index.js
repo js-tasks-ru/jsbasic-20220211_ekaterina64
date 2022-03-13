c20e94a1c8b71b8fd9129e9e80c8d6969da2df01
evborovik <ekaterina.borovik@t-systems.com>
2022-03-13 23:54:01 +0300

    TASKS for lesson #4 (1-3)

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,11 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let tRows = Array.from(table.rows);
+  tRows.forEach(row => {
+    let tCells = Array.from(row.cells);
+    tCells.forEach(cell => {
+      if (row.rowIndex === cell.cellIndex) {
+        cell.style.backgroundColor = 'red';
+      }
+    });
+  });
 }
