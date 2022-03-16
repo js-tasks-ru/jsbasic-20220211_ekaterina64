ac736a1bf366eaeefc0849cc2015123d49219bb6
evborovik <ekaterina.borovik@t-systems.com>
2022-03-16 15:28:25 +0300

    TASKS for lesson #5 (1-3)

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,37 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector(".carousel");
+  const slides = document.querySelectorAll(".carousel__slide");
+  const bLeft = document.querySelector(".carousel__arrow_left");
+  const bRight = document.querySelector(".carousel__arrow_right");
+  let px = 0;  
+
+  if (carousel) {
+    // init visibility of buttons
+    bLeft.style.display = "none";
+    if (slides.length <= 1) {
+      bRight.style.display = "none";
+      return;
+    }
+    // init onClick behaviour
+    bLeft.addEventListener("click", () => {
+      move("+");
+    });
+    bRight.addEventListener("click", () => {
+      move("-");
+    });
+  }
+
+  function move(direction) {
+    if (slides) {
+      const carouselInner = document.querySelector(".carousel__inner");
+      const carouselWidth = slides[0].offsetWidth;
+      px = eval(`${px}${direction}${carouselWidth}`);
+      carouselInner.style.transform = `translateX(${px}px)`;
+      bLeft.style.display = px === 0 ? "none" : "";
+      bRight.style.display =
+        px === -(slides.length - 1) * carouselWidth ? "none" : "";
+    }
+  }
 }
+
+
