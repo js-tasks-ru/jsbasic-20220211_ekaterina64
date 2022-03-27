44a6d2eeaff38b85eb9f2a6df2ef9494f38c7585
evborovik <ekaterina.borovik@t-systems.com>
2022-03-27 18:06:41 +0300

    TASKS for lesson #6 (1-3)

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,39 @@
+import createElement from "../../assets/lib/create-element.js";
+
 export default class ProductCard {
+  #product = null;
+  elem;
+
   constructor(product) {
+    this.#product = product;
+    this.elem = createElement(this.#cardTemplate());
+    const buttonAdd = this.elem.querySelector(".card__button");
+    buttonAdd.addEventListener("click", (event) => {
+      const eve = new CustomEvent("product-add", {
+        detail: this.#product.id, // Уникальный идентификатора товара из объекта товара
+        bubbles: true, // это событие всплывает - это понадобится в дальнейшем
+      });
+      console.log(eve);
+      event.target.dispatchEvent(eve);
+    });
+  }
+
+  #cardTemplate() {
+    return `
+    <div class="card">
+    <div class="card__top">
+        <img src="/assets/images/products/${
+          this.#product.image
+        }" class="card__image" alt="product">
+        <span class="card__price">€${this.#product.price.toFixed(2)}</span>
+    </div>
+    <div class="card__body">
+        <div class="card__title">${this.#product.name}</div>
+        <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+    </div>
+</div>
+`;
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,7 +1,76 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
+import ProductCard from "./slide.js";
 
 export default class Carousel {
+  elem = null;
+  #slides = null;
+  #bLeft = null;
+  #bRight = null;
+  #carouselInner = null;
+  #px = 0;
+
   constructor(slides) {
-    this.slides = slides;
+    this.elem = createElement(this.#templateCarousel());
+    this.#carouselInner = this.elem.querySelector(".carousel__inner");
+
+    slides.forEach((slide) => {
+      let productSlide = new ProductCard(slide);
+      this.#carouselInner.appendChild(productSlide.elem);
+    });
+    this.#initCarouselButtons();
+  }
+
+  #initCarouselButtons() {
+    
+    this.#slides = this.elem.querySelectorAll(".carousel__slide");
+    this.#bLeft = this.elem.querySelector(".carousel__arrow_left");
+    this.#bRight = this.elem.querySelector(".carousel__arrow_right");
+    
+    //if (carousel) {
+    // init visibility of buttons
+    this.#bLeft.style.display = "none";
+    if (this.#slides.length <= 1) {
+      this.#bRight.style.display = "none";
+      return;
+    }
+    // init onClick behaviour
+    this.#bLeft.addEventListener("click", () => {
+      move("+", this);
+    });
+    this.#bRight.addEventListener("click", () => {
+      move("-", this);
+    });
+    //}
+
+    function move(direction, classLnk) {
+      console.log(classLnk.#px);
+      if (classLnk.#slides) {
+        const carouselWidth = classLnk.#slides[0].offsetWidth;
+        
+        console.log(classLnk.elem);
+        classLnk.#px = eval(`${classLnk.#px}${direction}${carouselWidth}`);
+        classLnk.#carouselInner.style.transform = `translateX(${classLnk.#px}px)`;
+        classLnk.#bLeft.style.display = classLnk.#px === 0 ? "none" : "";
+        classLnk.#bRight.style.display =
+          classLnk.#px === -(classLnk.#slides.length - 1) * carouselWidth
+            ? "none"
+            : "";
+      }
+      console.log(classLnk.#px);
+    }
+  }
+
+  #templateCarousel() {
+    return `<!--Корневой элемент карусели-->
+    <div class="carousel">
+      <!--Кнопки переключения-->
+      <div class="carousel__arrow carousel__arrow_right">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </div>
+      <div class="carousel__arrow carousel__arrow_left">
+        <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+      </div>
+      <div class="carousel__inner"/>
+    </div>`;
   }
 }
